27894aa6458df0febf8de42cf2758a6eaefcd3f0
Aneil Mallavarapu <aneilmallavar@gmail.com>
2017-06-30 13:07:04 -0700

    Use parent node_module/.bin/babel to transpile after install

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,5 +1,5 @@
 echo "> Start transpiling ES2015"
 echo ""
-./node_modules/.bin/babel src --out-dir lib
+../../node_modules/.bin/babel src --out-dir lib
 echo ""
 echo "> Complete transpiling ES2015"
