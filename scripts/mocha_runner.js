541a983c83a4c72b4f348d39de63b7899fd1ca3a
Aneil Mallavarapu <aneilmallavar@gmail.com>
2017-07-23 10:32:03 -0700

    Fix mocha_runner.js script for CircleCI

--- scripts/mocha_runner.js
+++ scripts/mocha_runner.js
@@ -3,7 +3,7 @@ require('babel-polyfill');
 const log = require('../src/log');
 const dotenv = require('dotenv');
 dotenv.config({silent:true});
-log.level = process.env.LOGGER_LEVEL;
+log.level = process.env.LOGGER_LEVEL || log.level;
 
 process.on('unhandledRejection', function (error) {
   log.error('Unhandled Promise Rejection:');
